d396c48653c2a335e998b37e5f3067e8fcd8671b
Rushiera <e-hentai@qq.com>
2020-07-12 17:32:13 +0800

    fix

--- assets/js/titleindex.js
+++ assets/js/titleindex.js
@@ -138,112 +138,17 @@ function fixsp(){
     setTimeout(function(){fixs.innerHTML="“ ”";},"18750");
     setTimeout(function(){fixs.innerHTML="“|”";},"19000");
     //  -----------------------------------------------------------
-    setTimeout(function(){fixs.innerHTML="“あ”";},"19160");
-    setTimeout(function(){fixs.innerHTML="“あ|”";},"19320");
-    setTimeout(function(){fixs.innerHTML="“あい";},"19480");
-    setTimeout(function(){fixs.innerHTML="“あい|”";},"19640");
-    setTimeout(function(){fixs.innerHTML="“あいつ”";},"19800");
-    setTimeout(function(){fixs.innerHTML="“あいつの”";},"19960");
-    setTimeout(function(){fixs.innerHTML="“あいつの|”";},"20120");
-    setTimeout(function(){fixs.innerHTML="“あいつのや”";},"20280");
-    setTimeout(function(){fixs.innerHTML="“あいつのや|”";},"20440");
-    setTimeout(function(){fixs.innerHTML="“あいつのやく”";},"20600");
-    setTimeout(function(){fixs.innerHTML="“あいつのやく|”";},"20760");
-    setTimeout(function(){fixs.innerHTML="“あいつのやくめ”";},"20920");
-    setTimeout(function(){fixs.innerHTML="“あいつのやくめ|”";},"21080");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目”";},"21240");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目|”";},"21400");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に”";},"21560");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に|”";},"21720");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目にく”";},"21880");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目にく|”";},"22040");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目にくら”";},"22200");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目にくら|”";},"22360");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比”";},"22520");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比|”";},"22680");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べ”";},"22840");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べ|”";},"23000");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれ”";},"23160");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれ|”";},"23480");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば”";},"23640");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば|”";},"23800");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、|”";},"23960");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>|”";},"24120");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>だ”";},"24280");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>だ|”";},"24440");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>だい”";},"24600");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>だい|”";},"24760");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>だいじ”";},"24920");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>だいじ|”";},"25080");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事”";},"25240");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事|”";},"25400");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事な|”";},"25560");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なこ”";},"25720");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なこ|”";},"25880");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なこと”";},"26040");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なこと|”";},"26200");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを”";},"26360");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを|”";},"26520");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことをな”";},"26680");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことをな|”";},"26840");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無”";},"27000");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無|”";},"27160");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無か”";},"27320");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無か|”";},"27480");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かった”";},"27320");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かった|”";},"27480");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな”";},"27640");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな|”";},"27800");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな.”";},"27960");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな.|”";},"28120");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな. ”";},"28620");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな.|”";},"29120");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな. ”";},"29620");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな.|”";},"30120");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな. ”";},"30620");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな.|”";},"31120");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな. ”";},"31620");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな.|”";},"32120");
 
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かったな|”";},"32170");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かった|”";},"32220");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無かっ|”";},"32270");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無か|”";},"32320");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを無|”";},"32370");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なことを|”";},"32420");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なこと|”";},"32470");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事なこ|”";},"32520");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事な|”";},"32570");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大事|”";},"32620");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば、<br>大|”";},"32670");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれば|”";},"32720");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べれ|”";},"32770");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比べ|”";},"32820");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に比|”";},"32870");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目に|”";},"32920");
-    setTimeout(function(){fixs.innerHTML="“あいつの役目|”";},"33020");
-    setTimeout(function(){fixs.innerHTML="“あいつの役|”";},"33070");
-    setTimeout(function(){fixs.innerHTML="“あいつの|”";},"33120");
-    setTimeout(function(){fixs.innerHTML="“あいつ|”";},"33170");
-    setTimeout(function(){fixs.innerHTML="“あい|”";},"33220");
-    setTimeout(function(){fixs.innerHTML="“あ|”";},"33270");
-    setTimeout(function(){fixs.innerHTML="“|”";},"33320");
-    setTimeout(function(){fixs.innerHTML="“ ”";},"33970");
-    setTimeout(function(){fixs.innerHTML="“|”";},"34670");
-    setTimeout(function(){fixs.innerHTML="“ ”";},"35370");
-    setTimeout(function(){fixs.innerHTML="“|”";},"36070");
-    setTimeout(function(){fixs.innerHTML="“ ”";},"36770");
- 
-       setTimeout(function(){fixsp();},"37000");
+
+
+       setTimeout(function(){fixsp();},"19120");
     
 }
+
+    //Whatever you do, you should hold on to that foolishly hopeful smile.
+    //It's down to each one of you to decide your own level of commitment
+
     //用投资者的思维去创造,以开发者的角度去分析,<br>让工匠精神贯彻生活中的每一个部分。
-
-    //あいつの役目に比べれば、<br>大事なことを無かったな.
-    //      やくめ くら     だいじ      な
-    //あいつの役目に比べれば、大事なことを無かったな.
-
-    //Welcome to here 
 
     // 先设置一个字符串数组 里面装着 完整的句子。
     // 定义一个空字符串 用来接收里面的句子
